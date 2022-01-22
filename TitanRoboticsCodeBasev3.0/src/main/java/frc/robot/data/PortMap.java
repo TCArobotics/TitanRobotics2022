cc37a9270f2a50b3a44e5037e837be8843c90714
TCArobotics <61090685+TCArobotics@users.noreply.github.com>
2022-01-22 08:33:10 -0700

    Update port map to 2022 robot values

--- TitanRoboticsCodeBasev3.0/src/main/java/frc/robot/data/PortMap.java
+++ TitanRoboticsCodeBasev3.0/src/main/java/frc/robot/data/PortMap.java
@@ -6,12 +6,12 @@ package frc.robot.data;
 public enum PortMap
 {
     GAMEPAD(0),
-    FRONTRIGHT(0),
-    REARRIGHT(1),
-    TURNTABLE(2),
-    SHOOTER(7),
-    FRONTLEFT(8),
-    REARLEFT(9);
+    FRONTRIGHT(1),
+    REARRIGHT(0),
+    TURNTABLE(5),
+    SHOOTER(9),
+    FRONTLEFT(3),
+    REARLEFT(2);
 
     public int portNumber;
     private PortMap(int _portNumber)
